53f61903f36351ba7d25699bbadd537818519fda
rusti_hammer_P <rusti.hammer.p@gmail.com>
2026-03-05 20:52:55 +0900

    Add March 2026 sample market data to darby dataset

---
+++ data/darby-data-2603.js
@@ -0,0 +1,17 @@
+// darby March sample data (extracted from previous monthly file)
+// Note: file created in working tree only — no commit performed.
+
+darbyRawData = [
+    [
+        { "name": "SPYD",      "date": "20260304", "price": 77035 },
+        { "name": "S&P500",    "date": "20260304", "price": 72519 },
+        { "name": "オルカン",   "date": "20260304", "price": 73697 },
+        { "name": "全米株式",   "date": "20260304", "price": 72615 },
+        { "name": "FANG+",     "date": "20260304", "price": 66782 },
+        { "name": "SOX",       "date": "20260304", "price": 79194 },
+        { "name": "ABB",       "date": "20260304", "price": 69967 },
+        { "name": "SOMPO123",  "date": "20260304", "price": 73108 },
+        { "name": "VYM",       "date": "20260304", "price": 76363 },
+        { "name": "レバナス",   "date": "20260304", "price": 69283 }
+    ],
+];
