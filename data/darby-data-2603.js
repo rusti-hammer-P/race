291ac385325a6911a76c3ff27f2ed9bb61fb06cf
rusti_hammer_P <rusti.hammer.p@gmail.com>
2026-03-05 22:09:54 +0900

    Add March 5, 2026 market data to darby dataset

--- data/darby-data-2603.js
+++ data/darby-data-2603.js
@@ -14,4 +14,16 @@ darbyRawData = [
         { "name": "VYM",       "date": "20260304", "price": 76363 },
         { "name": "レバナス",   "date": "20260304", "price": 69283 }
     ],
+    [
+        { "name": "SPYD",      "date": "20260305", "price": 76134 },
+        { "name": "S&P500",    "date": "20260305", "price": 72577 },
+        { "name": "オルカン",   "date": "20260305", "price": 73562 },
+        { "name": "全米株式",   "date": "20260305", "price": 72659 },
+        { "name": "FANG+",     "date": "20260305", "price": 67435 },
+        { "name": "SOX",       "date": "20260305", "price": 80162 },
+        { "name": "ABB",       "date": "20260305", "price": 70014 },
+        { "name": "SOMPO123",  "date": "20260305", "price": 73177 },
+        { "name": "VYM",       "date": "20260305", "price": 75994 },
+        { "name": "レバナス",   "date": "20260305", "price": 70903 }
+    ],
 ];
