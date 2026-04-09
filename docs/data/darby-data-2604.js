423420911f747d3deb8498eee8cd817bd9c469fe
rusti_hammer_P <rusti.hammer.p@gmail.com>
2026-04-10 06:24:38 +0900

    Add April 8-9, 2026 sample data for darby

--- docs/data/darby-data-2604.js
+++ docs/data/darby-data-2604.js
@@ -36,5 +36,29 @@ darbyRawData = [
         { "name": "SOMPO123",  "date": "20260407", "price": 82590 },
         { "name": "VYM",       "date": "20260407", "price": 85532 },
         { "name": "レバナス",   "date": "20260407", "price": 76849 }
+    ],
+    [
+        { "name": "SPYD",      "date": "20260408", "price": 82598 },
+        { "name": "S&P500",    "date": "20260408", "price": 80889 },
+        { "name": "オルカン",   "date": "20260408", "price": 82520 },
+        { "name": "全米株式",   "date": "20260408", "price": 81063 },
+        { "name": "FANG+",     "date": "20260408", "price": 75909 },
+        { "name": "SOX",       "date": "20260408", "price": 92282 },
+        { "name": "ABB",       "date": "20260408", "price": 76814 },
+        { "name": "SOMPO123",  "date": "20260408", "price": 82175 },
+        { "name": "VYM",       "date": "20260408", "price": 85122 },
+        { "name": "レバナス",   "date": "20260408", "price": 76445 }
+    ],
+    [
+        { "name": "SPYD",      "date": "20260409", "price": 84531 },
+        { "name": "S&P500",    "date": "20260409", "price": 83058 },
+        { "name": "オルカン",   "date": "20260409", "price": 84877 },
+        { "name": "全米株式",   "date": "20260409", "price": 83238 },
+        { "name": "FANG+",     "date": "20260409", "price": 77935 },
+        { "name": "SOX",       "date": "20260409", "price": 98235 },
+        { "name": "ABB",       "date": "20260409", "price": 79239 },
+        { "name": "SOMPO123",  "date": "20260409", "price": 84518 },
+        { "name": "VYM",       "date": "20260409", "price": 86970 },
+        { "name": "レバナス",   "date": "20260409", "price": 80925 }
     ]
 ];
