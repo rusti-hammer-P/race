d30d2dd2bb90b6020d803e35e3ccb73e84ecf30b
rusti_hammer_P <rusti.hammer.p@gmail.com>
2026-07-07 22:42:13 +0900

    Add new race data for July 7, 2026, including various stocks and their prices

--- docs/data/derby-data-2607.js
+++ docs/data/derby-data-2607.js
@@ -24,5 +24,17 @@ derbyRawData = [
         { "name": "SOMPO123",  "date": "20260706", "price": 124656 },
         { "name": "VYM",       "date": "20260706", "price": 123890 },
         { "name": "レバナス",   "date": "20260706", "price": 141080 }
+    ],
+    [
+        { "name": "SPYD",      "date": "20260707", "price": 124050 },
+        { "name": "S&P500",    "date": "20260707", "price": 125196 },
+        { "name": "オルカン",   "date": "20260707", "price": 126130 },
+        { "name": "全米株式",   "date": "20260707", "price": 125768 },
+        { "name": "FANG+",     "date": "20260707", "price": 123580 },
+        { "name": "SOX",       "date": "20260707", "price": 183295 },
+        { "name": "ABB",       "date": "20260707", "price": 119542 },
+        { "name": "SOMPO123",  "date": "20260707", "price": 125495 },
+        { "name": "VYM",       "date": "20260707", "price": 124828 },
+        { "name": "レバナス",   "date": "20260707", "price": 145460 }
     ]
 ];
