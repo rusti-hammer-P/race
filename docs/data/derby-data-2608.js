7aee644387e2aab1764355dd06397235d95fcc25
rusti_hammer_P <rusti.hammer.p@gmail.com>
2026-08-05 22:33:18 +0900

    Add new derby data for October 5, 2026, including various stocks and their prices

---
+++ docs/data/derby-data-2608.js
@@ -0,0 +1,16 @@
+// derby October sample data
+
+derbyRawData = [
+    [
+        { "name": "SPYD",      "date": "20260805", "price": 132414 },
+        { "name": "S&P500",    "date": "20260805", "price": 134924 },
+        { "name": "オルカン",   "date": "20260805", "price": 134703 },
+        { "name": "全米株式",   "date": "20260805", "price": 135161 },
+        { "name": "FANG+",     "date": "20260805", "price": 138646 },
+        { "name": "SOX",       "date": "20260805", "price": 178184 },
+        { "name": "ABB",       "date": "20260805", "price": 128511 },
+        { "name": "SOMPO123",  "date": "20260805", "price": 135436 },
+        { "name": "VYM",       "date": "20260805", "price": 135208 },
+        { "name": "レバナス",   "date": "20260805", "price": 150049 }
+    ]
+];
